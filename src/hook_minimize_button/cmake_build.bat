62f280b73dec184557577d2cde0d7a112c478827
TsXor <zhang050525@qq.com>
2023-07-01 20:28:10 +0800

    improve cmake script

--- src/hook_minimize_button/cmake_build.bat
+++ src/hook_minimize_button/cmake_build.bat
@@ -2,11 +2,6 @@
 chcp 65001
 cd /d %~dp0
 
-if "%1"=="" (rem) else (
-    echo 你需要指定编译版本
-    goto END
-)
-
 if not exist build (mkdir build)
 if not exist build\x64 (mkdir build\x64)
 if not exist build\x32 (mkdir build\x32)
@@ -16,7 +11,7 @@ echo make 32bit
 echo **********
 cd build\x32
 cmake -G"Visual Studio 17 2022" -A Win32 -D ARCH=x32 ..\..
-cmake --build . --config %1
+cmake --build . --target install
 cd ..\..
 
 echo **********
@@ -24,9 +19,7 @@ echo make 64bit
 echo **********
 cd build\x64
 cmake -G"Visual Studio 17 2022" -A x64   -D ARCH=x64 ..\..
-cmake --build . --config %1
+cmake --build . --target install
 cd ..\..
-
-xcopy /Y run.bat dist\%1\
 
 :END
